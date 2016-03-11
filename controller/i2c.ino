79876f8319f07eb618651fcd4225447673b19995
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2016-03-11 13:05:41 +0100

    Fix the i2c controller

--- controller/i2c.ino
+++ controller/i2c.ino
@@ -4,6 +4,7 @@
 
 const int SLAVE_ADDRESS = 0x09;
 const int MAX_REGS = 16;
+
 volatile int8_t regs[MAX_REGS] = {};
 volatile uint8_t address;
 
@@ -15,21 +16,18 @@ void i2c_setup() {
 
 void receive(int bytes) {
     address = Wire.read();
-    while (Wire.available()) {
+    while (Wire.available() && address < MAX_REGS) {
         regs[address] = Wire.read();
         ++address;
-        if (address >= MAX_REGS) {
-            return;
-        }
     }
 }
 
 void i2c_loop() {
     for (int leg = 0; leg < 3; ++leg) {
         if (regs[leg * 4]) {
-            move_leg(leg, regs[leg * 4 + 1],
-                          regs[leg * 4 + 2],
-                          regs[leg * 4 + 3]);
+            move_leg(leg, regs[leg * 4 + 1] + HOME,
+                          regs[leg * 4 + 2] + HOME ,
+                          regs[leg * 4 + 3] - TIBIA);
             regs[leg * 4] = 0;
         }
     }
