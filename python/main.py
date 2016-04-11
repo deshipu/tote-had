c3c4775ee0362c727caae522e3eff6cbc225fa6e
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2016-04-11 22:25:04 +0200

    Don't wait for the connection

--- python/main.py
+++ python/main.py
@@ -1,9 +1,1 @@
-import network
-import server
-
-wlan = network.WLAN(network.STA_IF)
-ap = network.WLAN(network.AP_IF)
-while not wlan.isconnected() and not ap.isconnected():
-    pass
-
 server.main()
--- python/server.py
+++ python/server.py
@@ -11,28 +11,31 @@ def main():
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setblocking(False)
     sock.bind(addr)
-    sock.listen(0)
+    sock.listen(1)
 
     robot = Robot()
     gait = Creep(robot)
+    run = gait.run()
 
     while True:
-        client, addr = sock.accept()
-        client.setblocking(False)
-        for frame in gait.run():
-            command = client.read(1)
-            if command == b' ':
-                gait.dx = 0
-                gait.dy = 0
-                gait.rotation = 0
-            elif command == b'w':
-                gait.dy = min(3, gait.dy + 0.25)
-            elif command == b's':
-                gait.dy = max(-3, gait.dy - 0.25)
-            elif command == b'a':
-                gait.rotation = min(math.pi/90, gait.rotation + math.pi/360)
-            elif command == b'd':
-                gait.rotation = max(-math.pi/90, gait.rotation - math.pi/360)
-
-
-
+        try:
+            client, addr = sock.accept()
+        except OSError:
+            next(run)
+        else:
+            client.setblocking(False)
+            while True:
+                command = client.read(1)
+                if command == b' ':
+                    gait.dx = 0
+                    gait.dy = 0
+                    gait.rotation = 0
+                elif command == b'w':
+                    gait.dy = min(3, gait.dy + 0.25)
+                elif command == b's':
+                    gait.dy = max(-3, gait.dy - 0.25)
+                elif command == b'a':
+                    gait.rotation = min(math.pi/90, gait.rotation + math.pi/360)
+                elif command == b'd':
+                    gait.rotation = max(-math.pi/90, gait.rotation - math.pi/360)
+                next(run)
