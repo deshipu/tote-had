e481a7dd400de61c9c442c457779574aaed838aa
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2016-04-14 10:05:51 +0200

    Close the sockets properly

--- python/server.py
+++ python/server.py
@@ -6,36 +6,43 @@ from gait import Creep
 
 
 def main():
+    robot = Robot()
+    gait = Creep(robot)
+    run = gait.run()
+
     sock = socket.socket()
     addr = socket.getaddrinfo("0.0.0.0", 1010)[0][4]
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setblocking(False)
     sock.bind(addr)
     sock.listen(1)
-
-    robot = Robot()
-    gait = Creep(robot)
-    run = gait.run()
-
-    while True:
-        try:
-            client, addr = sock.accept()
-        except OSError:
-            next(run)
-        else:
-            client.setblocking(False)
-            while True:
-                command = client.read(1)
-                if command == b' ':
-                    gait.dx = 0
-                    gait.dy = 0
-                    gait.rotation = 0
-                elif command == b'w':
-                    gait.dy = min(3, gait.dy + 0.25)
-                elif command == b's':
-                    gait.dy = max(-3, gait.dy - 0.25)
-                elif command == b'a':
-                    gait.rotation = min(math.pi/90, gait.rotation + math.pi/360)
-                elif command == b'd':
-                    gait.rotation = max(-math.pi/90, gait.rotation - math.pi/360)
+    try:
+        while True:
+            try:
+                client, addr = sock.accept()
+            except OSError:
                 next(run)
+            else:
+                try:
+                    client.setblocking(False)
+                    while True:
+                        command = client.read(1)
+                        if command == b' ':
+                            gait.dx = 0
+                            gait.dy = 0
+                            gait.rotation = 0
+                        elif command == b'w':
+                            gait.dy = min(3, gait.dy + 0.25)
+                        elif command == b's':
+                            gait.dy = max(-3, gait.dy - 0.25)
+                        elif command == b'a':
+                            gait.rotation = min(math.pi/90,
+                                                gait.rotation + math.pi/360)
+                        elif command == b'd':
+                            gait.rotation = max(-math.pi/90,
+                                                gait.rotation - math.pi/360)
+                        next(run)
+                finally:
+                    client.close()
+    finally:
+        sock.close()
